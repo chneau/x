2f1ac7a3978517fa43eb6e01613639ed7f58baa1
chneau <charles63500@gmail.com>
2024-09-08 14:25:08 +0100

    Refactor CLI entry point and package.json

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-#!/usr/bin/env bun
+import { Command } from "commander";
+const program = new Command();
 
-console.log("Hello via Bun!");
+const version = await Bun.file(`${import.meta.dir}/package.json`)
+	.json()
+	.then((x) => x.version);
+
+program //
+	.name("x")
+	.description("chneau's utility CLI")
+	.version(version);
+
+program.parse();
