02b3bbc9cb01e729945aaf306044108709630683
chneau <charles63500@gmail.com>
2024-09-08 14:40:18 +0100

    Refactor version retrieval and add latest version check

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env bun
 import { Command } from "commander";
+import { fetchLatestVersion } from "./fetchLatestVersion";
 
 const program = new Command();
 
 const version = await Bun.file(`${import.meta.dir}/package.json`)
 	.json()
 	.then((x) => x.version as string);
+
+(async () => {
+	const latestVersion = await fetchLatestVersion();
+	if (latestVersion !== version)
+		console.log(`A new version is available: ${latestVersion}`);
+})();
 
 program //
 	.name("x")
