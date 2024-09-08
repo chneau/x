b2353288bdaccf21e0e112bb0e91a9837dc587f8
chneau <charles63500@gmail.com>
2024-09-08 20:13:53 +0100

    Refactor commandDoctor.ts to improve code readability and add user group, dotfiles, and system update checks

--- commandDoctor.ts
+++ commandDoctor.ts
@@ -3,53 +3,68 @@ import { canSudo, isRoot } from "./helpers";
 import { pkgs } from "./pkgs";
 
 const doctorRoot = async () => {
-	if (await isRoot()) console.log("❌ You are root");
-	else console.log("✅ You are not root");
+	const ok = !(await isRoot());
+	if (ok) console.log("✅ You are not root");
+	else console.log("❌ You are root");
+	return ok;
 };
 
 const doctorSudo = async () => {
-	if (await canSudo()) console.log("✅ You can sudo");
+	const ok = await canSudo();
+	if (ok) console.log("✅ You can sudo");
 	else console.log("❌ You cannot sudo");
+	return ok;
 };
 
 const doctorPkgs = async () => {
 	const result = await Promise.all(
 		pkgs.map(async (pkg) => ({ name: pkg.name, exists: await pkg.check() })),
 	).then((x) => x.filter((y) => !y.exists));
-	if (result.length === 0) {
+	const ok = result.length === 0;
+	if (ok) {
 		console.log("✅ All packages are installed");
 	} else {
 		console.log("❌ Some packages are not installed");
 		console.table(result);
 	}
+	return ok;
 };
 
 const doctorGitconfig = async () => {
-	const gitconfig = await $`git config --list`.text();
-	if (gitconfig.includes("user.email") && gitconfig.includes("user.name")) {
+	const gitconfig = await $`git config --list --global`.text();
+	const ok = ["user.email", "user.name", "url.ssh"].every((x) =>
+		gitconfig.includes(x),
+	);
+	if (ok) {
 		console.log("✅ Git config is set");
 	} else {
 		console.log("❌ Git config is not set");
 	}
+	return ok;
 };
 
 const doctorDotfiles = async () => {
 	const dotfiles = await $`ls -a $HOME`.text();
-	const expected = [".bashrc", ".zshrc", ".aliases", ".profile"];
-	if (expected.every((x) => dotfiles.includes(x))) {
+	const ok = [".bashrc", ".zshrc", ".aliases", ".profile"].every((x) =>
+		dotfiles.includes(x),
+	);
+	if (ok) {
 		console.log("✅ Dotfiles are installed");
 	} else {
 		console.log("❌ Dotfiles are not installed");
 	}
+	return ok;
 };
 
 const doctorUserGroups = async () => {
 	const groups = await $`groups`.text();
-	if (groups.includes("sudo") && groups.includes("docker")) {
+	const ok = ["sudo", "docker"].every((x) => groups.includes(x));
+	if (ok) {
 		console.log("✅ User is in sudo and docker groups");
 	} else {
 		console.log("❌ User is not in sudo and docker groups");
 	}
+	return ok;
 };
 
 export const commandDoctor = async () => {
