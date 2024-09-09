3cd0da81cd228b8b39aa0aae8112a9429a74ee12
chneau <charles63500@gmail.com>
2024-09-09 10:59:33 +0100

    Refactor commandDoctor.ts to update environment PATH variable

--- commandDoctor.ts
+++ commandDoctor.ts
@@ -2,30 +2,25 @@ import { $ } from "bun";
 import { canSudo, isRoot } from "./helpers";
 import { pkgs } from "./pkgs";
 
-$.env({
-	...Bun.env,
-	PATH: [
-		Bun.env.PATH ?? "",
-		"/home/linuxbrew/.linuxbrew/bin",
-		"/home/linuxbrew/.linuxbrew/sbin",
-		"$PULUMI_INSTALL/bin",
-		"$DENO_INSTALL/bin",
-		"$BUN_INSTALL/bin",
-		"$HOME/go/bin",
-		"$HOME/.arkade/bin",
-		"${KREW_ROOT:-$HOME/.krew}/bin",
-		"$HOME/.cargo/bin",
-		"$HOME/.dotnet",
-		"$HOME/.dotnet/tools",
-		"$HOME/.go/bin",
-		"$HOME/.local/bin",
-		"$HOME/bin",
-		"/snap/bin",
-		"/usr/local/sbin",
-		"/usr/sbin",
-		"/sbin",
-	].join(":"),
-});
+Bun.env.PATH = [
+	Bun.env.PATH ?? "",
+	"/home/linuxbrew/.linuxbrew/bin",
+	"/home/linuxbrew/.linuxbrew/sbin",
+	"$BUN_INSTALL/bin",
+	"$HOME/go/bin",
+	"$HOME/.arkade/bin",
+	"${KREW_ROOT:-$HOME/.krew}/bin",
+	"$HOME/.cargo/bin",
+	"$HOME/.dotnet",
+	"$HOME/.dotnet/tools",
+	"$HOME/.go/bin",
+	"$HOME/.local/bin",
+	"$HOME/bin",
+	"/snap/bin",
+	"/usr/local/sbin",
+	"/usr/sbin",
+	"/sbin",
+].join(":");
 
 const doctorRoot = async () => {
 	if (!(await isRoot())) console.log("✅ You are not root");
