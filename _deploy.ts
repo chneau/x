57130d157bfe44afabe81cf7f10281715fcb3e6c
chneau <charles63500@gmail.com>
2025-03-21 18:58:44 +0000

    Update build command in _deploy.ts and remove unused _test.ts file

--- _deploy.ts
+++ _deploy.ts
@@ -8,7 +8,7 @@ if (!latestVersionFix) throw new Error("latestVersionFix is not a number");
 const version = `0.0.${latestVersionFix + 1}`;
 
 await $`rm -rf dist`;
-await $`bun build --outfile=dist/x.js --target=bun --sourcemap=inline --minimify index.ts`;
+await $`bun build --outfile=dist/x.js --target=bun --production index.ts`;
 await Bun.write(
 	"dist/package.json",
 	JSON.stringify({ name: "@chneau/x", version, bin: "./x.js" }),
